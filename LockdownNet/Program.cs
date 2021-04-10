5ccc4ac40de8f5c7adadc3274a372a75ab9b5c63
Edison Fernández <AXEL1988@users.noreply.github.com>
2021-04-09 22:41:46 -0500

    Add code example

--- LockdownNet/Program.cs
+++ LockdownNet/Program.cs
@@ -1,12 +1,16 @@
 ﻿namespace LockdownNet
 {
     using System;
-    
+    using System.IO;
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var arguments = string.Join("; ", args);
+            var directory = Directory.GetCurrentDirectory();
+            Console.WriteLine($"Hola me estoy ejecutando en: {directory}");
+            Console.WriteLine(arguments);
         }
     }
 }
