8d261240219b185bde2987ff706cfb29aa5922b4
Edison Fernández <AXEL1988@users.noreply.github.com>
2021-05-13 07:27:24 -0500

    buidCommand

--- LockdownNet/Commands/BuildCommand.cs
+++ LockdownNet/Commands/BuildCommand.cs
@@ -11,9 +11,15 @@
             this.console = console;
         }
 
+        [Option("-r | --root")]
+        public string InputPath {get; set;} = "./";
+
+        [Option("-o | --output")]
+        public string OutputPath { get; set; } = "./_site";
         public int OnExecute()
         {
-            this.console.WriteLine($"Pleas do not use, alpha");
+            this.console.WriteLine($"Inputpath {InputPath}");
+            this.console.WriteLine($"Output {OutputPath}");
             return 0;
         }
     }
